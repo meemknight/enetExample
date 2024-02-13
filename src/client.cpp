16037f692f4352c46b4c45f55f60e4a7d56c2af5
vlod <vladalex420@gmail.com>
2024-02-13 15:45:12 +0200

    small fix

--- src/client.cpp
+++ src/client.cpp
@@ -12,7 +12,7 @@ void clientFunction()
 
 	client = enet_host_create(NULL /* create a client host */,
 		1 /* only allow 1 outgoing connection */,
-		2 /* allow up 2 channels to be used, 0 and 1 */,
+		CHANNELS /* allow up 2 channels to be used, 0 and 1 */,
 		0 /* assume any amount of incoming bandwidth */,
 		0 /* assume any amount of outgoing bandwidth */);
 
